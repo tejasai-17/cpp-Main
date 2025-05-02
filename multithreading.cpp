1fb2d960e6594b9e25bd27781d032aed3fc8f728
tejasai-17 <s.tejasai.17@gmail.com>
2025-05-02 18:36:02 +0530

    New content

--- multithreading.cpp
+++ multithreading.cpp
@@ -15,13 +15,22 @@ void function2(){
     
 }
 
+void function3(){
+    for (int i = 0; i < 200; i++){
+        std::cout << "C";
+    }
+    
+}
+
 int main(){
 
     std::thread person1(function1,'A');
     std::thread person2(function2);
+    std::thread person3(function3);
 
     person1.join();
     person2.join();
+    person3.join();
 
     return 0;
 }
